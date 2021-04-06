3051c9015220b11eb7df10564b9ba7d841dcf748
Nitoriubar <81606992+Nitoriubar@users.noreply.github.com>
2021-04-07 00:30:19 +0900

    Create closest_pair.py

---
+++ closest_pair.py
@@ -0,0 +1,20 @@
+# 제곱근 사용을 위한 sqrt 함수
+from math import sqrt
+
+# 두 매장의 직선 거리를 계산해 주는 함수
+def distance(store1, store2):
+    return sqrt((store1[0] - store2[0]) ** 2 + (store1[1] - store2[1]) ** 2)
+
+# 가장 가까운 두 매장을 찾아주는 함수
+def closest_pair(coordinates):
+    min = 999
+    for i in range(len(coordinates)):
+        for j in range(len(coordinates)):
+            if distance(coordinates[i],coordinates[j]) < min:
+                result = coordinates[i],coordinates[j]
+        else:
+            break
+    return list(result)
+
+test_coordinates = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
+print(closest_pair(test_coordinates))
