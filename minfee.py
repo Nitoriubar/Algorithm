d6c344329a6a5fb5f1d1af09f67e76faca03ab38
Nitoriubar <81606992+Nitoriubar@users.noreply.github.com>
2021-04-07 00:23:48 +0900

    Create minfee.py

---
+++ minfee.py
@@ -0,0 +1,16 @@
+def min_fee(pages_to_print):
+    pages_to_print = sorted(pages_to_print)
+    j=0
+    s=0
+    su=[]
+    for i in range(len(pages_to_print)):
+       if j==i:
+           s = s + pages_to_print[i]
+           su.append(s)
+           j += 1
+    return sum(su) 
+# 테스트
+print(min_fee([6, 11, 4, 1]))
+print(min_fee([3, 2, 1]))
+print(min_fee([3, 1, 4, 3, 2]))
+print(min_fee([8, 4, 2, 3, 9, 23, 6, 8]))
