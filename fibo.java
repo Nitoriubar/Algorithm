d302b2a483a1e5ef97474ae64bf5a6201c27b33c
Nitoriubar <81606992+Nitoriubar@users.noreply.github.com>
2021-04-14 11:31:33 +0900

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -2,14 +2,15 @@ import java.util.*;
 
 public class Fibo {
   
-  public int fibo(int n) {
-    if(n==1 || n==2){
-      return 1;
-    }
-    return fibo(n-1) + fibo(n-2); 
-  }
+   long[] fibo_array = new long[100];
   
-  public static void main(String[] args) {
+   public static void main(String[] args) {
+     for(int i=1; i<=n; i++){
+       if(n == 1 || n==2){
+         fibo_array[i] = 1;
+       }
+       fibo_array[i] = fibo_array[i-1] + fibo_array[i-2];
+     }
      System.out.println(fibo(100));
   }
 }
