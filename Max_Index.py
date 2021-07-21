c97b9d2b6e459b24604d5d0059a4aedaa6a52bb8
Nitoriubar <81606992+Nitoriubar@users.noreply.github.com>
2021-07-21 16:51:48 +0900

    Update Max_Index.py

--- Max_Index.py
+++ Max_Index.py
@@ -1,9 +1,11 @@
 def max_index(a):
-	max = a[0]
-	for i in range(1,len(a)-1):
-		if a[i] > max :
-			max = a[i]
+    max = a[0]
+    for i in range(1,len(a)-1):
+	if a[i] > max :
+	    max = a[i]
 	return a.index(max)
 
 a = [17,92,18,33,58,7,33,42]
 print(max_index(a))
+
+
